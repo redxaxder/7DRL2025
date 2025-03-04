cc0f96729951bb74486c1b886c135a1664b5c046
redxaxder <abyaly@gmail.com>
2025-03-04 15:14:55 -0600

    more keys

--- src/input.rs
+++ src/input.rs
@@ -16,14 +16,28 @@ static mut INPUT_MAP: &mut [(KeyCode, Input)] = &mut [
   (KeyCode::A, Input::Dir(Left)),
   (KeyCode::S, Input::Dir(Down)),
   (KeyCode::D, Input::Dir(Right)),
-  (KeyCode::Up, Input::Dir(Up)),
-  (KeyCode::Left, Input::Dir(Left)),
-  (KeyCode::Down, Input::Dir(Down)),
-  (KeyCode::Right, Input::Dir(Right)),
   (KeyCode::Q, Input::Rotate1),
   (KeyCode::E, Input::Rotate2),
   (KeyCode::Z, Input::LevelUp),
   (KeyCode::X, Input::Discard),
+
+  (KeyCode::K, Input::Dir(Up)),
+  (KeyCode::H, Input::Dir(Left)),
+  (KeyCode::J, Input::Dir(Down)),
+  (KeyCode::L, Input::Dir(Right)),
+  (KeyCode::Y, Input::Rotate1),
+  (KeyCode::U, Input::Rotate2),
+  (KeyCode::B, Input::LevelUp),
+  (KeyCode::N, Input::Discard),
+
+  (KeyCode::Up, Input::Dir(Up)),
+  (KeyCode::Left, Input::Dir(Left)),
+  (KeyCode::Down, Input::Dir(Down)),
+  (KeyCode::Right, Input::Dir(Right)),
+  (KeyCode::Comma, Input::Rotate1),
+  (KeyCode::Period, Input::Rotate2),
+  (KeyCode::Enter, Input::LevelUp),
+  (KeyCode::Backspace, Input::Discard),
 ];
 
 pub fn get_input() -> Option<Input> {
